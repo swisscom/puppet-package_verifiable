653d80839ec223be9a9d7248013d3664870c6ac0
Marcel Haerry <marcel.haerry@swisscom.com>
2013-12-12 13:10:52 +0100

    remove debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,7 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
-  puts c.pattern
   c.pattern = FileList[c.pattern].exclude(/^spec\/fixtures/)
-  puts c.pattern
 end
 
 Puppet::Util::Log.level = :warning
